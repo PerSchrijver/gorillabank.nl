efa92819b39e90f031295f6bb9f51aae565808c7
PerSchrijver <perschrijver@gmail.com>
2025-06-30 14:58:24 +0200

    Fix logo

---
+++ backend/static/js/toggle-sidebar.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const btn  = document.getElementById('hamburger');
+
+  btn.addEventListener('click', () => {
+    document.body.classList.toggle('sidebar-collapsed');
+    document.body.classList.toggle('sidebar-expanded');
+  });
+});
